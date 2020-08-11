9a224825ebc407c96ab7b2754e215a9184353074
AmshenShanu <65121810+AmshenShanu07@users.noreply.github.com>
2020-08-11 11:41:40 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -25,7 +25,7 @@ im a group manager bot maintained by  [this person](tg://user?id={}).
 
 For more commands click /help...
 
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
+**[I am](https://t.me/Green_hat_07) the actuall owner of this bot code. if there is any quarys u can message me .**
 
 """
 
@@ -49,7 +49,7 @@ DONATE_STRING = """Heya, glad to hear you want to donate!
 It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+There are two ways of paying him; [PayPal](paypal.me), or [Monzo](monzo.me)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
